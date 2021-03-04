2d4ea977e6fa9489fa1ad966d4ef68d0c5021d63
idolevitt <idolevit2@gmail.com>
2021-03-04 22:49:38 +0200

    uploading images for the readme.txt

--- src/main.java
+++ src/main.java
@@ -4,13 +4,13 @@ public class main {
 
     public static void main(String[] args) {
 
-        //String url = "src/input.jpg";
-        //BufferedImage image = GreyScale.greyScale(url);
+        String url = "src/input.jpg";
+        Invert.invertColors(url);
         //Image.writeImage(image , "output.png");
 
 
         //for (int i = 0; i < 150; i++) {
-            SmartCrop.cropPath("output.jpg" , "output.jpg");
+        //    SmartCrop.cropPath("input.jpg", "input.jpg");
         //}
 
     }
